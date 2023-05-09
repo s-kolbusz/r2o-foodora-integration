6f77f7accd4d230fb2020d8feddaf0601dd70922
Sebastian Kolbusz <sebastian.kolbusz@ready2order.com>
2023-05-09 13:56:55 +0200

    add api routes

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default async function Home() {
+  return <h2 className="text-2xl font-semibold drop-shadow">Orders list:</h2>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,18 +9,15 @@ export default async function Home() {
     <main className="flex min-h-screen flex-col items-center p-24 space-y-4">
       <div className="flex space-x-8 justify-between items-center">
         <h1 className="text-3xl font-bold drop-shadow-md">
-          Welcome to the r2o food delivery service integration.
+          Welcome to the r2o foodora delivery service integration.
         </h1>
 
         <button
           onClick={onSyncButtonClick}
           className="px-4 py-2 bg-pink-500 hover:bg-pink-400 text-pink-50 rounded-md"
         >
-          Sync with foodora
+          SYNC
         </button>
-      </div>
-      <div className="w-full">
-        <h2 className="text-2xl font-semibold drop-shadow">Orders list:</h2>
       </div>
     </main>
   );
